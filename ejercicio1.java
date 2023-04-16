3c3fe9db5d0367f9e18a0a7b2c985aa0a1a44504
MaxiPerezx <maxmanul@live.com>
2023-04-15 22:51:37 -0300

    tp4

---
+++ ejercicio1.java
@@ -0,0 +1,18 @@
+package tp4.ejercicio1;
+
+
+import java.util.Scanner;
+public class ejercicio1 {public static void main (String[] args){
+	Scanner lectura = new Scanner (System.in);{
+
+		System.out.println("Ingrese su nombre: ");
+		String nombre = lectura.next();
+		System.out.println("Ingrese su edad: ");
+		int edad = lectura.nextInt();
+		System.out.println("Su nombre es: " + nombre + " y su edad es: " + edad);}
+
+}//cierra método main
+
+}//cierra class
+
+
